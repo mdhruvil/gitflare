e5079ba17fbab06b444f985b575b4c764d8c4bef
mdhruvil <dhruvil1808@gmail.com>
2025-12-16 10:04:09 +0530

    feat: add better auth config and drizzle schema

---
+++ apps/web/src/lib/auth.ts
@@ -0,0 +1,29 @@
+import { env } from "cloudflare:workers";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { apiKey, username } from "better-auth/plugins";
+import { db } from "@/db";
+
+export const auth = betterAuth({
+  // baseURL: env.SITE_URL,
+  trustedOrigins: [env.SITE_URL],
+  database: drizzleAdapter(db, {
+    provider: "sqlite",
+  }),
+  emailAndPassword: {
+    enabled: true,
+    requireEmailVerification: false,
+  },
+  advanced: {
+    useSecureCookies: true,
+  },
+  plugins: [
+    username(),
+    apiKey({
+      enableMetadata: true,
+      rateLimit: {
+        enabled: false,
+      },
+    }),
+  ],
+});
