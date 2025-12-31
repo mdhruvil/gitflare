662618bed933bb66840755c98ab188b311caccf1
mdhruvil <git@mdhruvil.com>
2025-12-31 22:30:30 +0530

    feat: nuke convex stuff

--- apps/web/src/lib/auth-server.ts
+++
@@ -1,6 +0,0 @@
-import { setupFetchClient } from "@convex-dev/better-auth/react-start";
-import { createAuth } from "@gitvex/backend/convex/auth";
-import { getCookie } from "@tanstack/react-start/server";
-
-export const { fetchQuery, fetchMutation, fetchAction } =
-  await setupFetchClient(createAuth, getCookie);
