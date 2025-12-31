c812e971defa4659030c661770bbedc8e9785ff0
mdhruvil <git@mdhruvil.com>
2025-12-31 17:55:49 +0530

    feat: migrate better auth from convext to d1

--- apps/web/src/lib/auth.ts
+++ apps/web/src/lib/auth.ts
@@ -2,10 +2,11 @@ import { env } from "cloudflare:workers";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { apiKey, username } from "better-auth/plugins";
+import { tanstackStartCookies } from "better-auth/tanstack-start";
 import { db } from "@/db";
 
 export const auth = betterAuth({
-  // baseURL: env.SITE_URL,
+  baseURL: env.SITE_URL,
   trustedOrigins: [env.SITE_URL],
   database: drizzleAdapter(db, {
     provider: "sqlite",
@@ -25,5 +26,6 @@ export const auth = betterAuth({
         enabled: false,
       },
     }),
+    tanstackStartCookies(),
   ],
 });
